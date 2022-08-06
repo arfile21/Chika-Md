14c16f4fa29862cabd51dc4b0a971f463cfa8c52
arfile21 <83054268+arfile21@users.noreply.github.com>
2022-08-06 09:41:38 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6282116364919','6282116364919','6282116364919'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6282116364919'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6282116364919' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'AROwner' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'ArBotz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = '4RDev' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = 'https://youtube.com'
+global.ig = 'https://instagram.com'
+global.mygc = 'wa.me/6282116364919'
+global.myweb = 'www.arstore.my.id'
+global.email = '4rdhan08@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© ARBotz' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = '4RDev' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 60,
 }
 
 //—————「 Set Image 」—————//
@@ -62,7 +62,7 @@ global.thumb = fs.readFileSync('./image/chika.jpg')
 
 //—————「 Set Image Welcome 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
+global.BgWelcLeav = 'https://arstore.my.id/wp-content/uploads/2022/05/cropped-ar-initial-logo-linked-circle-monogram-vector-29216704-1.jpg'
 
 //—————「 Set Random Image Menu 」—————//
 //gausah di apa² in!
